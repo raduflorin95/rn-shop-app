968f289f3a33b4930dc47386bdc27fb4d62806ca
Radu Florin <raduflorin95@MacBook-Air-Radu.local>
2020-11-12 23:22:46 +0200

    Added fonts and adjusted design in product item

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import { FlatList, Text } from "react-native";
 import { useSelector } from "react-redux";
 
+import ProductItem from "../../components/shop/ProductItem";
+
 const ProductsOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
 
@@ -9,13 +11,26 @@ const ProductsOverviewScreen = (props) => {
     <FlatList
       data={products}
       keyExtractor={(item) => item.id}
-      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {
+            props.navigation.navigate("ProductDetail", {
+              productId: itemData.item.id,
+              productTitle: itemData.item.title,
+            });
+          }}
+          onAddToCart={() => {}}
+        />
+      )}
     />
   );
 };
 
 ProductsOverviewScreen.navigationOptions = {
-  headerTitle: "Al Products",
+  headerTitle: "All Products",
 };
 
 export default ProductsOverviewScreen;
