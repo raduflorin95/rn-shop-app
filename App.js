f02ff732a600120e2108c80690a98995a8e4fef3
Radu Florin <raduflorin95@MacBook-Air-Radu.local>
2020-11-15 01:19:04 +0200

    Add current progress - Cart & Orders screen and the corresponding store parts

--- App.js
+++ App.js
@@ -5,10 +5,15 @@ import { AppLoading } from "expo";
 import * as Font from "expo-font";
 
 import productsReducer from "./store/reducers/products";
+import cartReducer from "./store/reducers/cart";
+import ordersReducer from "./store/reducers/order";
+
 import ShopNavigator from "./navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
   products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer,
 });
 
 const store = createStore(rootReducer);
---
+++ store/reducers/cart.js
@@ -0,0 +1,64 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
+import CartItem from "../../models/cart-item";
+
+const initialState = {
+  items: {},
+  totalAmount: 0,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.product;
+      const prodPrice = addedProduct.price;
+      const prodTitle = addedProduct.title;
+
+      let updatedOrNewCartItem;
+
+      if (state.items[addedProduct.id]) {
+        // already have the item in the cart
+        updatedOrNewCartItem = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          prodPrice,
+          prodTitle,
+          state.items[addedProduct.id].sum + prodPrice
+        );
+      } else {
+        updatedOrNewCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+      }
+
+      return {
+        ...state,
+        items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
+        totalAmount: state.totalAmount + prodPrice,
+      };
+
+    case REMOVE_FROM_CART:
+      const productId = action.productId;
+      const selectedProduct = state.items[productId];
+      const currentQuantity = selectedProduct.quantity;
+
+      let updatedCartItems;
+
+      if (currentQuantity > 1) {
+        const updatedCartItem = new CartItem(
+          currentQuantity - 1,
+          selectedProduct.productPrice,
+          selectedProduct.productTitle,
+          selectedProduct.sum - selectedProduct.productPrice
+        );
+        updatedCartItems = { ...state.items, [productId]: updatedCartItem };
+      } else {
+        updatedCartItems = { ...state.items };
+        delete updatedCartItems[productId];
+      }
+
+      return {
+        ...state,
+        items: updatedCartItems,
+        totalAmount: state.totalAmount - selectedProduct.productPrice,
+      };
+  }
+
+  return state;
+};
